976d03e0041cb032b798dd05e94f09ac6562f7d0
Maikiller <maikiller-a7x@hotmail.com>
2021-04-09 15:48:49 -0300

    implements routes,templete..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, TemplateModule, ClientesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
